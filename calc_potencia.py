e9e49ffe2f76331ed69b3f7da01e0fbc18d54cb8
Luciana <luuhnunes2003@gmail.com>
2026-05-26 14:10:27 -0400

    feat: implementa potência e seus testes

---
+++ calc_potencia.py
@@ -0,0 +1,7 @@
+# calc_potencia.py
+# Módulo B — Potência
+# Autor: Luciana
+# Branch: feature/modulo-potencia
+
+def potencia(base, expoente):
+    return base ** expoente
--- test_calc.py
+++ test_calc.py
@@ -23,3 +23,13 @@ class TestCalcConversao(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
+
+from calc_potencia import potencia
+
+def test_potencia():
+    assert potencia(2, 3) == 8
+    assert potencia(5, 0) == 1
+    assert potencia(3, 4) == 81 
